311d4d10294d9e544522d02cd59726613cefec38
hollidavis <hollidavis19@gmail.com>
2021-06-30 15:04:57 -0600

    added images for choices, worked on js for play

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-function play(playerChoice) {
-
+let options = {
+  rock: {
+    value: 1,
+    img: 'rock.png'
+  },
+  paper: {
+    value: 2,
+    img: 'paper.png'
+  },
+  scissors: {
+    value: 3,
+    img: 'scissors.png'
+  }
 }
+
+function play(playerChoice) {
+  let choice = options[playerChoice]
+  let compChoice = 'rock'
+  // compChoice will eventually equal a function that chooses randomly
+  document.getElementById('choice-pic').innerHTML = `<img class="w-100" src="${choice.img}" alt="${playerChoice}">`
+}
+
+//document.getElementById('compChoice-pic').innerHTML = `<img class="w-100" src="${options.img}" alt="${compChoice}">`
+//document.getElementById("speak-output").innerText = `The ${animalName} says ${animal.says}.`
